5e2bb0a532b4911b9f0039aa3e8740bde1af56f1
Evan <koopevan@gmail.com>
2022-07-27 16:26:01 -0600

    Final Commit

--- lab.js
+++ lab.js
@@ -7,12 +7,18 @@
 */
 
 //CODE HERE
+// let lovesCode = true
 
-// For problems 2-3 use the following lines of code:
-var amysAge = 29;
-var brittanisAge = 34;
-var amysBirthYear = 1991;
-var brittanisBirthYear = 1986;
+// if(lovesCode = true){
+//   console.log('I love to code!')
+// } else{
+//   console.log('Coding has its challenges.')
+// }
+// // For problems 2-3 use the following lines of code:
+// var amysAge = 29;
+// var brittanisAge = 34;
+// var amysBirthYear = 1991;
+// var brittanisBirthYear = 1986;
 
 ////////// PROBLEM 2 //////////
 
@@ -21,6 +27,15 @@ var brittanisBirthYear = 1986;
 */
 
 //CODE HERE
+// if(amysAge > brittanisAge){
+//   console.log('Amy is older')
+// } else if (amysAge < brittanisAge)
+// {
+//   console.log('Brittani is older')
+// } else {
+//   console.log('Brittani and Amy have the same age')
+// }
+
 
 ////////// PROBLEM 3 //////////
 
@@ -30,18 +45,51 @@ var brittanisBirthYear = 1986;
 
 //CODE HERE
 
+// if (brittanisBirthYear === amysBirthYear)
+// {
+//   console.log('Amy and Brittani were born in the same birth year')
+// }
+// else {
+//   console.log('Amy and Brittani were not born in the same birth year')
+// }
 ////////// PROBLEM 4 //////////
 
-let temperature = 55;
-let rain = true;
+// let temperature = 55;
+// let rain = true;
 
-/*
-  Using an if statement and a comparison operator, create code that will log a suggestion on what type of clothes one should wear for the day, based on the above temperature and rain.
-  If it is 80 degrees or above, and raining, one should wear a t-shirt and take an umbrella. If it is between 60 and 80 degrees, and raining, one should wear a rain-jacket. If it is 60 degrees or below, one should wear a jacket and carry an umbrella. Come up with your own clothing suggestions based on those temperature parameters when it is not raining.
-*/
+// /*
+//   Using an if statement and a comparison operator, create code that will log a suggestion on what type of clothes one should wear for the day, based on the above temperature and rain.
+//   If it is 80 degrees or above, and raining, one should wear a t-shirt and take an umbrella. If it is between 60 and 80 degrees, and raining, one should wear a rain-jacket. If it is 60 degrees or below, one should wear a jacket and carry an umbrella. Come up with your own clothing suggestions based on those temperature parameters when it is not raining.
+// */
 
-//CODE HERE
+// //CODE HERE
+// if(temperature > 80 && rain === true){
+//   console.log('wear t-shirt and take an umbrella')
 
+// }  
+// else if(temperature < 80 && 60 >= temperature  && rain === true){
+//   console.log('wear a rainjacket')
+// }
+// else if (temperature < 60 && rain === true)
+// console.log('wear a rainjacket and carry an umbrella') && rain === true
+
+// if(temperature > 80 && rain === false){
+//   console.log('wear t-shirt')
+
+// }  
+// else if(temperature < 80 && 60 >= temperature  && rain === false){
+//   console.log('wear a jacket')
+// }
+// else if(temperature < 60 && rain === false){
+//   console.log('wear a jacket')
+// }
+ 
+ 
+
+
+
+
+  
 ////////// PROBLEM 5 //////////
 
 /*
@@ -49,13 +97,17 @@ let rain = true;
 */
 
 //CODE HERE
-
+// for(let i = 0; i < 10; i++ ){
+//   console.log('hello')
+// }
 ////////// PROBLEM 6 //////////
 
 /*
   Create a for-loop that runs 10 times, logging the numbers 1 through 10. Ex, on the first iteration it would log 1, on the second iteration it would log 2.
 */
-
+// for(let i = 1; i <= 10; i++){
+//   console.log(i)
+// }
 //CODE HERE
 
 ////////// PROBLEM 7 //////////
@@ -63,19 +115,25 @@ let rain = true;
 /*
   Create a for-loop that logs the numbers 0 through 10 backwards. Ex, on the first iteration it would log 10, on the second iteration it would log 9.
 */
-
+// for(let i = 10; i >= 0; i--){
+//   console.log(i)
+// }
 //CODE HERE
 
 
 ////////// PROBLEM 8 //////////
 
-let score = 0;
-let passingScore = 7;
+// let score = 0;
+// let passingScore = 7;
 
-/*
-  Create a while-loop that logs "Your score is not high enough" while the score variable above is below passingScore. In each iteration of the loop, increase the score by one. If your code is successful, you will see your logged statement 7 times.
-*/
-
+// /*
+//   Create a while-loop that logs "Your score is not high enough" while the score variable above is below passingScore. In each iteration of the loop, increase the score by one. If your code is successful, you will see your logged statement 7 times.
+// */
+// while (score < passingScore)
+// {
+//   console.log('Your score is not high enough');
+//   score++
+// }
 //CODE HERE
 
 ////////// INTERMEDIATE PROBLEMS //////////
@@ -88,17 +146,36 @@ let passingScore = 7;
 */
 
 //CODE HERE
+// let changeMyMind = false
 
+// if(changeMyMind === true){
+//   console.log('true')
+// } 
+// else if(changeMyMind === false){
+//   console.log('false')
+// }
+  
 ////////// PROBLEM 10 //////////
 // Using the "not" operator (!), change the current value of changeMyMind to true (it should currently be set to false due to the if-else statement in Problem 9). After you change the value of changeMyMind, console.log it's new value (it should now read true).
 
 //CODE HERE
+// let changeMyMind = false
 
+// if(changeMyMind !== true){
+//   console.log('true')
+// } 
+// else if(changeMyMind !== false){
+//   console.log('false')
+// }
 ////////// ADVANCED PROBLEMS //////////
 
 ////////// PROBLEM 11 //////////
 
-let z = 5;
-// Create a while loop that continues to run while z is greater than 0. Within the while loop, log a countdown from the value of z to 1. Once you have logged the countdown, make sure you decrement z by 1. If your code is running properly, you should see: 5,4,3,2,1,4,3,2,1,3,2,1,2,1,1.
+// let z = 5;
+// // Create a while loop that continues to run while z is greater than 0. Within the while loop, log a countdown from the value of z to 1. Once you have logged the countdown, make sure you decrement z by 1. If your code is running properly, you should see: 5,4,3,2,1,4,3,2,1,3,2,1,2,1,1.
 
-//CODE HERE
+// //CODE HERE
+// while (z > 0) {
+//   console.log(z);
+//   --z
+// }
